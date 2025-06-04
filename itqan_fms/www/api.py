3fdf4f7182fb0d58e6219e7130e232ff9a27f9d6
Abdulrhman Khaled <bodyono3@gmail.com>
2025-06-04 12:58:15 +0300

    return desc with type

--- itqan_fms/www/api.py
+++ itqan_fms/www/api.py
@@ -270,10 +270,7 @@ def get_priority_list():
 
 @frappe.whitelist()
 def get_issue_type_list():
-    l = frappe.db.get_all("Issue Type", fields = ["name","description"], as_list = 1)
-    if l: l = list(flatten(l))
-
-    return l
+    return frappe.db.get_all("Issue Type", fields = ["name","description"], as_list = 1)
 
 @frappe.whitelist()
 def get_profile_info(user):
